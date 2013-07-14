02a9673ad4d47a8ee5ed50ce8b335f82b07cde88
Stephan Köninger <mail@stephan-koeninger.de>
2013-07-14 18:55:49 +0200

    Added validation to registration form.

--- src/main/java/de/stekoe/idss/component/form/registration/RegistrationForm.java
+++ src/main/java/de/stekoe/idss/component/form/registration/RegistrationForm.java
@@ -1,37 +1,159 @@
 package de.stekoe.idss.component.form.registration;
 
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.EmailTextField;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.PasswordTextField;
-import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.RequiredTextField;
+import org.apache.wicket.markup.html.form.validation.EqualPasswordInputValidator;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.StringResourceModel;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 
+import de.agilecoders.wicket.core.markup.html.bootstrap.form.ControlGroup;
 import de.stekoe.idss.component.behavior.Placeholder;
 import de.stekoe.idss.model.User;
 
 @SuppressWarnings("serial")
 public class RegistrationForm extends Panel {
+	
+	private static final Logger LOG = Log.getLogger(RegistrationForm.class);
 
 	private User user = new User();
-	
+
+	private Form<User> form;
+
+	private RequiredTextField<String> usernameField;
+	private EmailTextField email;
+	private PasswordTextField password;
+	private PasswordTextField passwordConfirm;
+	private Button submitButton;
+
 	public RegistrationForm(String id) {
 		super(id, null);
-		
-		Form<User> form = new Form<User>("registrationForm"){
+		createFields();
+		createRegistrationForm();
+	}
+
+	private void createFields() {
+		setUsernameField();
+		setEmailField();
+		setPasswordField();
+		setPasswordConfirmField();
+		setSubmitButton();
+	}
+
+	private void createRegistrationForm() {
+		form = new Form<User>("registrationForm"){
 			@Override
 			protected void onSubmit() {
-				User user = (User) this.getModelObject();
-				System.out.println(user);
+				User user = getModelObject();
+				LOG.info(user.toString());
 			}
 		};
 		form.setModel(new CompoundPropertyModel<User>(user));
 		
-		TextField<String> username = new TextField<String>("username");
-		username.add(new Placeholder("form.field.username.placeholder", this));
-		form.add(username);
+		form.add(getUsernameControlGroup());
+		form.add(getEmailControlGroup());
+		form.add(getPasswordControlGroup());
+		form.add(getPasswordConfirmControlGroup());
+		form.add(getButtonControlGroup());
 		
-		form.add(new PasswordTextField("password"));
-		
+		form.add(getPasswordsEqualBehavior());
+
 		add(form);
+	}
+
+	private ControlGroup getUsernameControlGroup() {
+		String id = "usernameControlGroup";
+		RequiredTextField<String> field = getUsernameField();
+		return createControlGroup(id, field);
+	}
+
+	private void setUsernameField() {
+		usernameField = new RequiredTextField<String>("username");
+		usernameField.setLabel(new StringResourceModel("username.label", this, null));
+		usernameField.add(new Placeholder("username.placeholder", this));
+	}
+
+	private RequiredTextField<String> getUsernameField() {
+		return usernameField;
+	}
+
+	private ControlGroup getEmailControlGroup() {
+		String id = "emailControlGroup";
+		EmailTextField field = getEmailField();
+		return createControlGroup(id, field);
+	}
+
+	private void setEmailField() {
+		email = new EmailTextField("email");
+		email.setRequired(true);
+		email.setLabel(new StringResourceModel("email.label", this, null));
+		email.add(new Placeholder("email.placeholder", this));
+	}
+
+	private EmailTextField getEmailField() {
+		return email;
+	}
+	
+	private ControlGroup getPasswordControlGroup() {
+		String id = "passwordControlGroup";
+		PasswordTextField field = getPasswordField();
+		return createControlGroup(id, field);
+	}
+
+	private PasswordTextField getPasswordField() {
+		return password;
+	}
+
+	private void setPasswordField() {
+		password = new PasswordTextField("password");
+		password.setLabel(new StringResourceModel("password.label", this, null));
+	}
+
+	private ControlGroup getPasswordConfirmControlGroup() {
+		String id = "passwordConfirmControlGroup";
+		PasswordTextField field = getPasswordConfirmField();
+		return createControlGroup(id, field);
+	}
+
+	private PasswordTextField getPasswordConfirmField() {
+		return passwordConfirm;
+	}
+
+	private void setPasswordConfirmField() {
+		passwordConfirm = new PasswordTextField("passwordConfirm", Model.of(""));
+		passwordConfirm.setLabel(new StringResourceModel("passwordConfirm.label", this, null));
+	}
+	
+	private ControlGroup getButtonControlGroup() {
+		String id = "buttonControlGroup";
+		Button submitButton = getSubmitButton();
+		return createControlGroup(id, submitButton);
+	}
+	
+	private Button getSubmitButton() {
+		return submitButton;
+	}
+	
+	private void setSubmitButton() {
+		submitButton = new Button("submit");
+		submitButton.setModel(new StringResourceModel("submit.label", this, null));
+	}
+
+	@SuppressWarnings("rawtypes")
+	private ControlGroup createControlGroup(String id, FormComponent... field) {
+		ControlGroup cg = new ControlGroup(id);
+		cg.add(field);
+		return cg;
+	}
+
+	private EqualPasswordInputValidator getPasswordsEqualBehavior() {
+		return new EqualPasswordInputValidator(getPasswordField(), getPasswordConfirmField());
 	}
 }

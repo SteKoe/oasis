a5b86fd076720b73116cb5e0905005336d812129
Stephan Köninger <mail@stephan-koeninger.de>
2013-07-22 18:11:11 +0200

    Added various error pages.

---
+++ src/main/java/de/stekoe/idss/page/error/Error403Page.java
@@ -0,0 +1,11 @@
+package de.stekoe.idss.page.error;
+
+import de.stekoe.idss.page.LayoutPage;
+
+/**
+ * Custom 403 HTTP Status class
+ */
+@SuppressWarnings("serial")
+public class Error403Page extends LayoutPage {
+
+}

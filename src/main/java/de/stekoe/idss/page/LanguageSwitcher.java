a5eb051d9e45dec28d7f27753c96ba70d4366eb6
Stephan Köninger <mail@stephan-koeninger.de>
2013-07-23 12:56:56 +0200

    Fixed compilation errors.

--- src/main/java/de/stekoe/idss/page/LanguageSwitcher.java
+++ src/main/java/de/stekoe/idss/page/LanguageSwitcher.java
@@ -1,43 +1,57 @@
 package de.stekoe.idss.page;
 
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+
+import de.stekoe.idss.IDSSSession;
 
 
 @SuppressWarnings("serial")
 public class LanguageSwitcher extends Panel {
 
-    private final Map<String, Locale> languages = new HashMap<String, Locale>();
+    private final List<Locale> languages = new ArrayList<Locale>();
 
     public LanguageSwitcher(String id) {
         super(id);
 
         setLanguages();
 
-        ListView loop = new ListView("languages", getLanguages().) {
+        ListView<Locale> loop = new ListView<Locale>("languages", getLanguages()) {
 
             @Override
-            protected void populateItem(ListItem item) {
-                // TODO Auto-generated method stub
+            protected void populateItem(final ListItem<Locale> item) {
+                Link link = new Link("language") {
+                    @Override
+                    public void onClick() {
+                        Locale key = item.getModelObject();
+                        IDSSSession.get().setLocale(key);
+                    }
+                };
 
+                link.add(new Label("languageName", Model.of(item.getModelObject())));
+
+                item.add(link);
             }
         };
+        loop.setRenderBodyOnly(true);
 
         add(loop);
     }
 
     private void setLanguages() {
-        languages.put("en", Locale.ENGLISH);
-        languages.put("de", Locale.GERMAN);
+        languages.add(Locale.ENGLISH);
+        languages.add(Locale.GERMAN);
     }
 
-    private Map<String, Locale> getLanguages() {
+    private List<Locale> getLanguages() {
         return languages;
     }
 

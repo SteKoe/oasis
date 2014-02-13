824d0d25c808767c14efb7f038c152fec116f0a2
Stephan Köninger <mail@stephan-koeninger.de>
2014-02-13 09:11:14 +0100

    Added missing annotations

--- idss-services/src/main/java/de/stekoe/idss/dao/impl/CriterionPageDAO.java
+++ idss-services/src/main/java/de/stekoe/idss/dao/impl/CriterionPageDAO.java
@@ -2,10 +2,12 @@ package de.stekoe.idss.dao.impl;
 
 import de.stekoe.idss.dao.ICriterionPageDAO;
 import de.stekoe.idss.model.criterion.CriterionPage;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Stephan Koeninger <mail@stephan-koeninger.de>
  */
+@Repository
 public class CriterionPageDAO extends GenericDAO<CriterionPage> implements ICriterionPageDAO {
     @Override
     protected Class getPersistedClass() {

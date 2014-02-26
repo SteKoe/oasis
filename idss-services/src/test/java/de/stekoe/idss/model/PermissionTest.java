f56840acb1b49f82d4493023a8361f64a1665300
Stephan Koeninger <mail@stephan-koeninger.de>
2014-02-26 16:38:56 +0100

    New PermissionTest

--- idss-services/src/test/java/de/stekoe/idss/model/PermissionTest.java
+++ idss-services/src/test/java/de/stekoe/idss/model/PermissionTest.java
@@ -6,6 +6,7 @@ import org.hamcrest.core.IsEqual;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -13,21 +14,6 @@ import static org.junit.Assert.assertTrue;
  * @author Stephan Koeninger <mail@stephan-koeninger.de>
  */
 public class PermissionTest {
-
-    private Permission permission;
-
-    @Before
-    public void setUp() {
-        this.permission = new Permission();
-    }
-
-    @Test
-    public void constructorTest() throws Exception {
-        final Permission permission = new Permission(PermissionObject.PROJECT, PermissionType.READ, "ObjectId");
-        assertThat(permission.getPermissionObject(), IsEqual.equalTo(PermissionObject.PROJECT));
-        assertThat(permission.getPermissionType(), IsEqual.equalTo(PermissionType.READ));
-        assertThat(permission.getObjectId(), IsEqual.equalTo("ObjectId"));
-    }
 
     @Test
     public void hasIdTest() throws Exception {
@@ -37,31 +23,8 @@ public class PermissionTest {
     }
 
     @Test
-    public void getId() throws Exception {
-        final String expected = "ABC";
-        permission.setId(expected);
-        assertThat(permission.getId(), IsEqual.equalTo(expected));
+    public void hasNoIdTest() throws Exception {
+        final Permission permission = new Permission(PermissionObject.PROJECT, PermissionType.CREATE, null);
+        assertFalse(permission.hasObjectId());
     }
-
-    @Test
-    public void getObjectId() throws Exception {
-        final String expected = "ObjectID";
-        permission.setObjectId(expected);
-        assertThat(permission.getObjectId(), IsEqual.equalTo(expected));
-    }
-
-    @Test
-    public void getObjectType() throws Exception {
-        final PermissionObject expected = PermissionObject.PROJECT;
-        permission.setPermissionObject(expected);
-        assertThat(permission.getPermissionObject(), IsEqual.equalTo(expected));
-    }
-
-    @Test
-    public void getPermissionType() throws Exception {
-        final PermissionType expected = PermissionType.CREATE;
-        permission.setPermissionType(expected);
-        assertThat(permission.getPermissionType(), IsEqual.equalTo(expected));
-    }
-
 }
